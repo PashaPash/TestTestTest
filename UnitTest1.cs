fca9decf3f22c5c6ecf084a1cd5c0d80e5b9d1af
Pavel Pochobut <pavel.pochobut@logicsoftware.net>
2021-04-14 13:21:57 +0300

    test project added

---
+++ UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestTestTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
