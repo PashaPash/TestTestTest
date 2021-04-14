03c32b4f5932ba1593a05b239736996089aea614
Pavel Pochobut <pavel.pochobut@logicsoftware.net>
2021-04-14 13:30:17 +0300

    test added

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -8,6 +8,14 @@ namespace TestTestTest
         [TestMethod]
         public void TestMethod1()
         {
+            // some1
+            var a = 1;
+
+            // some2
+            var b = 1;
+
+            // assert
+            // Assert.AreEqual(2, a + b);
         }
     }
 }
