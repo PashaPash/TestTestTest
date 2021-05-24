23cf9ea83ab6482770bd63ff26dbc600e7939d8e
Pavel Pochobut <pavel.pochobut@gmail.com>
2021-05-24 13:18:55 +0300

    Update UnitTest1.cs

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -14,8 +14,8 @@ namespace TestTestTest
             // change to 2 in pr1
             var b = 1;
 
-            // uncomment in pr2
-            Assert.AreEqual(2, a + b);
+            // uncomment in master
+            // Assert.AreEqual(2, a + b);
             
             // wait for both pr validation build
             // merge pr1
